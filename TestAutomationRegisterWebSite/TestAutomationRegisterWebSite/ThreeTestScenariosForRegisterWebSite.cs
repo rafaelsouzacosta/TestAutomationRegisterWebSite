ae96483fb415bf9c54a67015969f6eeb15a0e0a5
Rafael Souza Costa <rafael.souza-costa@hotmail.com>
2020-04-15 16:14:33 -0300

    First Test
    Still need to organize to pageObject, will do that once the 3 test are runing

---
+++ TestAutomationRegisterWebSite/TestAutomationRegisterWebSite/ThreeTestScenariosForRegisterWebSite.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace TestAutomationRegisterWebSite
+{
+    public class ThreeTestScenariosForRegisterWebSite
+    {
+        private IWebDriver driver;
+        public string homeURL;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            homeURL = "http://demo.automationtesting.in/Register.html";
+            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+        }
+
+        [Test(Description = "Scenario:The application display the required message on Last Name")]
+        public void required_message_last_name_field()
+        {
+            driver.Navigate().GoToUrl(homeURL);
+            WebDriverWait wait = new WebDriverWait(driver, System.TimeSpan.FromSeconds(15));
+
+            IWebElement FirstName = driver.FindElement(By.CssSelector("[ng-model='FirstName']"));
+            FirstName.SendKeys("Test");
+            IWebElement LastName = driver.FindElement(By.CssSelector("[ng-model='LastName']"));
+            LastName.SendKeys("");
+
+            IWebElement SubmitButton = driver.FindElement(By.Id("submitbtn"));
+            SubmitButton.Click();
+
+            Assert.AreEqual("true", LastName.GetAttribute("required"));
+        }
+
+        [TearDown]
+        public void TearDownTest()
+        {
+            driver.Close();
+        }
+    }
+}
